7c93b8f556f0f6b4e7619f917ba4863425d007d2
shandey007 <jhefersonbm.query@gmail.com>
2026-01-05 15:41:40 -0500

    Mi pagina esta previo a crear a editar la pagina EditCarta, es mi checkpoint

--- sistema_servicio/src/pages/ClientesPage.jsx
+++ sistema_servicio/src/pages/ClientesPage.jsx
@@ -252,14 +252,29 @@ function ClientesPage() {
                 onClick={handleAddCliente}
                 className="btn btn-sm btn-primary"
               >
-                + Nuevo Cliente
+                  <svg
+                  width="18"
+                  height="18"
+                  fill="none"
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                  >
+                  <rect width="18" height="18" x="3" y="3" rx="2" ry="2" />
+                  <path d="M12 8v8" />
+                  <path d="M8 12h8" />
+                </svg>
+                <span className='hidden md:inline'>Nuevo Cliente</span>
               </button>
             )}
           </div>
           
           <form onSubmit={handleSubmit}>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <div className="form-control">
+            <div className="grid grid-cols-1 md:grid-cols-5 gap-4 lg:gap-2 text-sm lg:text-base mb-10">
+              <div className="form-control flex flex-col">
                 <label className="label">
                   <span className="label-text">Nombres *</span>
                 </label>
@@ -296,7 +311,7 @@ function ClientesPage() {
                 />
               </div>
               
-              <div className="form-control">
+              <div className="form-control flex flex-col">
                 <label className="label">
                   <span className="label-text">Celular</span>
                 </label>
@@ -309,8 +324,8 @@ function ClientesPage() {
                 />
               </div>
               
-              <div className="form-control">
-                <label className="label">
+              <div className="form-control flex flex-col">
+                <label className="label justify-between">
                   <span className="label-text">DNI *</span>
                 </label>
                 <input
@@ -322,9 +337,6 @@ function ClientesPage() {
                   maxLength={12}
                   placeholder="Ej: 76045247"
                 />
-                <label className="label">
-                  <span className="label-text-alt">Debe tener entre 8 y 12 caracteres</span>
-                </label>
               </div>
               
               <div className="form-control">
@@ -443,23 +455,51 @@ function ClientesPage() {
                         {cliente.fecha_registro ? new Date(cliente.fecha_registro).toLocaleDateString('es-PE') : ''}
                       </td>
                       <td>
-                        <span className={`badge ${cliente.estado ? 'badge-success' : 'badge-error'}`}>
+                        <span className={`badge ${cliente.estado ? 'badge-success' : 'badge-neutral'}`}>
                           {cliente.estado ? 'Activo' : 'Inactivo'}
                         </span>
                       </td>
                       <td>
-                        <div className="flex flex-col sm:flex-row gap-2">
+                        <div className="flex flex-row sm:flex-row gap-2">
                           <button
                             onClick={() => handleEditCliente(cliente)}
                             className="btn btn-sm btn-primary"
                           >
-                            Editar
+                            <svg
+                                    width="18"
+                                    height="18"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                            >
+                            <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
+                            <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
+                            </svg>
+                            <span className="hidden sm:inline">Editar</span>
                           </button>
                           <button
                             onClick={() => handleDeleteCliente(cliente.id_cliente)}
                             className="btn btn-sm btn-error"
                           >
-                            Eliminar
+                            <svg
+                                    width="18"
+                                    height="18"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    viewBox="0 0 24 24"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                  >
+                                    <path d="M3 6h18" />
+                                    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+                            </svg>
+                            <span className="hidden sm:inline">Eliminar</span>
                           </button>
                         </div>
                       </td>
