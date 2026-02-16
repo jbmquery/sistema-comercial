e1121e3dcaec9d070d5e32f420ac0d76179278d4
shandey007 <jhefersonbm.query@gmail.com>
2026-02-16 02:33:56 -0500

    Recuperé lso botones editar y eliminar

--- sistema_servicio/src/pages/OrdenPage.jsx
+++ sistema_servicio/src/pages/OrdenPage.jsx
@@ -465,7 +465,54 @@ function OrdenPage() {
                             <td>{d.observacion}</td>
 
                             <td className="flex gap-2">
-                              {/* BOTONES QUE YA TENÍAS */}
+                              <button
+                                className="btn btn-sm btn-info btn-square"
+                                onClick={() => {
+                                  setDetalleEditar(d);
+                                  setObsEditar(d.observacion || "");
+                                  setMostrarModalEditar(true);
+                                }}
+                              >
+                                {/* Boton Editar */}
+                                <svg
+                                  width="16"
+                                  height="16"
+                                  fill="none"
+                                  stroke="currentColor"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth="2"
+                                  viewBox="0 0 24 24"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
+                                  <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
+                                </svg>
+                              </button>
+
+                              <button
+                                className="btn btn-sm btn-secondary btn-square"
+                                onClick={() => {
+                                  setDetalleABorrar(d);
+                                  setMostrarModalBorrar(true);
+                                }}
+                              >
+                                {/* Boton Borrar */}
+                                <svg
+                                  width="18"
+                                  height="18"
+                                  fill="none"
+                                  stroke="currentColor"
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth="2"
+                                  viewBox="0 0 24 24"
+                                  xmlns="http://www.w3.org/2000/svg"
+                                >
+                                  <path d="M3 6h18" />
+                                  <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+                                </svg>
+                              </button>
                             </td>
                           </tr>
                         ))}
